e9f524757749cd742e24c91231b47e8061467b16
NgQuVi_1410 <vinguyenkg123@gmail.com>
2026-06-12 14:04:36 +0700

    ui chuan hoa theo dac ta

--- UI/build_ui_demo.bat
+++ UI/build_ui_demo.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+set "PATH=C:\msys64\mingw32\bin;%PATH%"
 nasm -f win32 demo_ui.asm -o demo_ui.o
 if errorlevel 1 exit /b 1
 gcc -m32 demo_ui.o -o ui_demo.exe -lkernel32
